b36fa164002bc9a7508c9c71b6326971d956a4b2
neeraj001122 <admobid0011>
2022-09-14 13:00:17 +0530

    advance js9

---
+++ task9.js
@@ -0,0 +1,54 @@
+var student = {name:'sidharth',
+age : 19};
+
+function get(a,b)
+{
+    return `Name is ${this.name+a} and age is ${this.age}`
+}
+
+console.log(get.call(student,' kumar'))
+
+var student = {name:'sidharth',
+age : 19};
+
+var arr = [1,6,9];
+
+function get(a)
+{
+    return `He is ${this.name} and age is ${this.age} and number is ${a}`
+}
+
+console.log(get.call(student,arr))
+
+var student = {name:'sidharth',
+age : 19};
+
+var arr = [1,5,69]
+
+function get(a)
+{
+    return `Name is ${this.name+a} and age is ${this.age}`
+}
+
+var bound = get.bind(student)
+
+console.log(bound(arr))
+
+function multiply(x,y)
+{
+     console.log(x*y)
+}
+
+var multiplybytwo = multiply.bind(this, 2);
+multiplybytwo(3)
+
+function multiply(x)
+{
+   return function mul(y)
+   {
+    console.log(x*y)
+   }
+}
+
+var multiplybytwo = multiply(2)
+multiplybytwo(2)
